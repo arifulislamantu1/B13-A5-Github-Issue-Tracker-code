b28bc5e0d2dd6a7aaba9acf27c6d48daed8a4a99
arifulislamantu1 <arifulislamantu1@gmail.com>
2026-03-09 23:47:47 +0600

    Add issue details modal for single issue view

--- script/home.js
+++ script/home.js
@@ -66,6 +66,7 @@ function displayIssues(issues) {
                           issue.priority.toLowerCase() === 'medium' ? 'btn-warning' : 'btn-info';
 
     card.innerHTML = `
+        <div onclick="openIssueModal(${issue.id})" class="space-y-4">
       <div class="flex justify-between">
         <img class="w-8 h-8" src="./assets/Open-Status.png" alt="">
         <p class="btn btn-soft ${priorityColor} rounded-full">${issue.priority.toUpperCase()}</p>
@@ -76,34 +77,63 @@ function displayIssues(issues) {
         <p class="text-[#64748b] text-[12px]">${issue.description}</p>
       </div>
 
-      <div class="flex gap-2 flex-wrap">${labelsHtml}</div>
+      <div class="flex gap-2 mt-2 mb-2 ${priorityColor}">${labelsHtml}</div>
 
       <div class="border border-[#e4e4e7] p-4 space-y-1 text-[#64748b] text-[12px]">
         <p>#${issue.id} by ${issue.author}</p>
         <p>${new Date(issue.createdAt).toLocaleDateString()}</p>
       </div>
+      </div>
     `;
 
     
-    card.addEventListener('click', () => openIssueModal(issue.id));
+    // card.addEventListener('click', () => openIssueModal(issue.id));
 
     container.appendChild(card);
   });
 }
 
+const openIssueModal = async (id) =>{
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
 
-async function openIssueModal(id) {
-  try {
-    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
-    const result = await res.json();
-    const issue = result.data;
-
-    alert(`Issue #${issue.id}\nTitle: ${issue.title}\nDescription: ${issue.description}\nStatus: ${issue.status}\nPriority: ${issue.priority}\nAuthor: ${issue.author}\nCreated: ${new Date(issue.createdAt).toLocaleString()}`);
-  } catch (err) {
-    console.error("Error fetching single issue:", err);
-  }
+    const res = await fetch(url);
+    const details = await res.json();
+    displayCardDetails(details.data);
+    
 }
 
+const displayCardDetails =(card) =>{
+    const detailsContainer = document.getElementById('details-container');
+
+    const labelsHtml = card.labels.map(label =>
+      `<p class="btn btn-soft btn-sm rounded-full">${label}</p>`
+    ).join(' ');
+
+    const priorityColor = card.priority.toLowerCase() === 'high' ? 'btn-error' :
+                          card.priority.toLowerCase() === 'medium' ? 'btn-warning' :'btn-info';
+                         
+    detailsContainer.innerHTML = `
+    <h1 class="font-bold text-[#1f2937]">${card.title}</h1>
+            <div class="flex gap-2">
+                <span id="status-card" class="text-[#64748b] text-[12px]">${card.status}</span> <p class="text-[#64748b] text-[12px]"> Opened by ${card.assignee}</p> <p class="text-[#64748b] text-[12px]">${card.updatedAt}</p>
+            </div>
+            <div class="flex gap-2 mt-2 mb-2 ${priorityColor}">${labelsHtml}</div>
+            <p class="text-[#64748b] text-[12px]">${card.description}</p>
+
+            <div class="flex  bg-slate-100 p-3 rounded-md">
+                <div class="flex-1">
+                <p class="text-[#64748b] text-[12px] space-y-1">Assignee:</p>
+                <p class="font-semibold text-[#1f2937]">${card.assignee}</p>
+                </div>
+                <div class="flex-1 space-y-1">
+                    <p class="text-[#64748b] text-[12px] pl-3">Priority:</p>
+                    <p class="btn btn-soft ${priorityColor} rounded-full">${card.priority.toUpperCase()}</p>
+                </div>
+                
+            </div>`;
+
+    document.getElementById('card_modal').showModal();
+}
 
 document.getElementById("searchInput").addEventListener("keyup", (e) => {
   if (e.key === "Enter") {
